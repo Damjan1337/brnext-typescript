3a3b62c708139d431bfaec5e644ead59c1516e39
Damjan1337 <88947531+Damjan1337@users.noreply.github.com>
2023-12-21 11:54:16 +0100

    Tasks completed
    Finish

--- src/03-email-extractor/index.ts
+++ src/03-email-extractor/index.ts
@@ -1,1 +1,20 @@
-export function extractEmails(users) {}
+interface User {
+  id: number
+  name: string
+  email: string
+  age: number
+}
+
+export function extractEmails(users: (User | null | undefined)[]): string[] {
+  const uniqueEmails = new Set<string>()
+
+  if (users && Array.isArray(users)) {
+    for (const user of users) {
+      if (user && user.email) {
+        uniqueEmails.add(user.email)
+      }
+    }
+  }
+
+  return Array.from(uniqueEmails)
+}
--- src/04-pascals-triangle/index.ts
+++ src/04-pascals-triangle/index.ts
@@ -1,1 +1,27 @@
-export function getPascalsTriangleRow(rowIndex) {}
+export function getPascalsTriangleRow(rowIndex: number): number[] {
+  if (rowIndex < 0) {
+    throw new Error('Row index must be non-negative')
+  }
+
+  const row: number[] = []
+
+  for (let i = 0; i <= rowIndex; i++) {
+    row.push(binomialCoefficient(rowIndex, i))
+  }
+
+  return row
+}
+
+// Helper function to calculate binomial coefficient C(n, k)
+function binomialCoefficient(n: number, k: number): number {
+  if (k === 0 || k === n) {
+    return 1
+  }
+
+  let result = 1
+  for (let i = 1; i <= k; i++) {
+    result *= (n - i + 1) / i
+  }
+
+  return Math.round(result)
+}
